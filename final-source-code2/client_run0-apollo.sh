3dec5b4c289dc99667787dcd71153fdebd720191
abhi4578 <apollo@smpc-server0.yd3ubk4jyvaepciwlzz202yo2b.rx.internal.cloudapp.net>
2025-01-21 09:04:16 +0000

    Updated filenames to match node numbers.

--- final-source-code2/client_run0-apollo.sh
+++ final-source-code2/client_run0-apollo.sh
@@ -104,4 +104,4 @@ gramine-sgx ./server "$mr_enclave" "$mr_signer" 0 0 >> "player_${KII_PLAYER_NUMB
 
 ./KII "$mr_enclave" "$mr_signer" 0 0 $KII_PLAYER_NUMBER >> "kii_${KII_PLAYER_NUMBER}.log" 2>&1 &
 
-
+box_out "END RUN $KII_PLAYER_NUMBER $KII_PLAYER_NAME"
