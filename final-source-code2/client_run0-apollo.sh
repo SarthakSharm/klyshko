cda48ca1d714f215877dd1c27ba734ccf7f6d16e
srishti <srishti.mittal@datakaveri.org>
2025-03-18 17:36:42 +0530

    minor change in scripts

--- final-source-code2/client_run0-apollo.sh
+++ final-source-code2/client_run0-apollo.sh
@@ -3,16 +3,16 @@
 if [ -f "kii_0.log" ]; then
     rm "kii_0.log"
     # echo "File removed successfully."
-else
+# else
     # echo "File does not exist."
-fi
+# fi
 
 if [ -f "kii_1.log" ]; then
     rm "kii_1.log"
     # echo "File removed successfully."
-else
+# else
     # echo "File does not exist."
-fi
+# fi
 
 rm -r Player-Data
 mkdir Player-Data
